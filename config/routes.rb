8bf4fc930336a1bbfe84250c75320a6d016883f7
kyuu5988 <kyuu5988@yahoo.co.jp>
2020-11-04 11:29:51 +0900

    comp ctrls 作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :comps
+  devise_for :comps, controllers: {
+    sessions:      'comps/sessions',
+    passwords:     'comps/passwords',
+    registrations: 'comps/registrations'
+  }
   devise_for :users
 
   root 'resumes#index'
